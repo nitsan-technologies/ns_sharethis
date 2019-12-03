9609e7a09fcf2072d57c667965e7afb27839e32c
NITSAN <sanjay@nitsan.in>
2019-12-03 11:02:57 +0530

    [TODO] Adding Gitlab-CI & Travis with PHP, TypoScript, YAML lints [TODO] Updating documentation [TODO] Added demo link [TODO] Releasing pro-version of the extenion at https://t3terminal.com/

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ns_sharethis
-The TYPO3 Extension: https://extensions.typo3.org/extension/ns_sharethis/
+
+- LIVE demo: https://demo.t3terminal.com/t3t-extensions/
+- FREE version: You can download FREE version with basic-features at https://extensions.typo3.org/extension/ns_sharethis/
+- PRO version: You can download PRO version with more-features & free-support at https://t3terminal.com/typo3-sharethis-plugin-free/
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,8 +1,8 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => '[NITSAN] Plugin for www.Sharethis.com',
-	'description' => 'One of the only TYPO3 extension is developed to integrate all the features of www.Sharethis.com which is one of the most popular third-party social media platform!',
+	'title' => '[NITSAN] Sharethis.com Plugin',
+	'description' => 'Easy to configure socially-share your TYPO3 site using www.Sharethis.com which is one of the most popular third-party social media platform! Live-Demo: https://demo.t3terminal.com/t3t-extensions/ You can download PRO version for more-features & free-support at https://t3terminal.com/typo3-sharethis-plugin-free/',
 	'category' => 'plugin',
 	'author' => 'T3:Bhavin Barad, T3:Keval Pandya, QA:Siddharth Sheth',
 	'author_email' => 'sanjay@nitsan.in',
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '1.1.2',
+	'version' => '1.2.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '6.0.0-8.9.99',
