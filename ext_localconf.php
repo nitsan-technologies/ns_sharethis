25d2bcf49a37c98562f41a79e432fc9e8a6f7e97
Stephan Neudecker <nitsan-gitlab@fischer-and-friends.de>
2021-12-23 11:10:45 +0530

    [TODO]version 11 compatibility

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,16 +2,21 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
-$_EXTKEY = 'ns_sharethis';
+// $_EXTKEY = 'ns_sharethis';
 
+if (version_compare(TYPO3_branch, '10.0', '>=')) {
+    $moduleClass = \Nitsan\NsSharethis\Controller\SharethisController::class;
+} else {
+    $moduleClass = 'Sharethis';
+}
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'Nitsan.' . $_EXTKEY,
+    'Nitsan.NsSharethis',
     'Nitsansharethis',
     [
-        'Sharethis' => 'list',
-        ],
+        $moduleClass => 'list',
+    ],
     // non-cacheble actions
     [
-        'Sharethis' => 'list',
-        ]
+        $moduleClass => 'list',
+    ]
 );
