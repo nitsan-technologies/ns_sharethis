0d2b3dfc8333366d2da391ec42af05720978eb40
Saloni Thakkar <saloni.nitsan@gmail.com>
2025-10-02 10:58:36 +0530

    [TASK]Changes Done Given in Review Sheet

--- Classes/Controller/SharethisController.php
+++ Classes/Controller/SharethisController.php
@@ -9,6 +9,31 @@ use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\PathUtility;
 
+/***************************************************************
+ *
+ *  Copyright notice
+ *
+ *  (c) 2023
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
 class SharethisController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 {
     /**
@@ -21,8 +46,8 @@ class SharethisController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
         $pageRenderer = GeneralUtility::makeInstance(PageRenderer::class);
 
         $extConf = $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['ns_sharethis'] ?? [];
-        if (! empty($extConf['csspath'])) {
-            $cssFileName = ltrim($extConf['csspath'], '/');
+        if (! empty($extConf['cssPath'])) {
+            $cssFileName = ltrim($extConf['cssPath'], '/');
             $assetPath   = Environment::isComposerMode()
                 ? $this->getPath('/', 'ns_sharethis')
                 : PathUtility::stripPathSitePrefix(ExtensionManagementUtility::extPath('ns_sharethis')) . 'Resources/Public/';
@@ -51,20 +76,12 @@ class SharethisController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
 
         if ($categoryHoverBar || $globalSharingEnabled) {
             $userScriptUrl = $extConf['url'] ?? '';
-            $userClass     = $extConf['class'] ?? 'sharethis-inline-share-buttons';
+            $userClass     = $extConf['class'] ?? '';
 
             if (! empty($userScriptUrl)) {
                 // Inject ShareThis JS
-                $pageRenderer->addHeaderData(
-                    '<script type="text/javascript" src="' . htmlspecialchars($userScriptUrl) . '" async></script>'
-                );
-
-                // Inject share buttons container with configurable class
-                $pageRenderer->addHeaderData('
-            <!-- ShareThis BEGIN -->
-            <div class="' . htmlspecialchars($userClass) . '"></div>
-            <!-- ShareThis END -->
-        ');
+                $pageRenderer->addHeaderData($userScriptUrl);
+                $pageRenderer->addHeaderData($userClass);
             }
         }
 
